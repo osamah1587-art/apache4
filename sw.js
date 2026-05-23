001db9651eb2cc2a0eb3f38be142d352349139d3
osamah1587-art <osamah1587@gmail.com>
2026-05-23 09:36:41 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 const CACHE = 'apache4-v1';
-const FILES = ['./index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
-
+const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
